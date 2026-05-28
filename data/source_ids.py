e02a965fd6d19a059d9a300391623a17ee757542
Kory Yang <kory.z.yang@gmail.com>
2026-05-28 16:57:59 -0700

    update train test split

--- data/source_ids.py
+++ data/source_ids.py
@@ -23,17 +23,32 @@ SOURCE_IDS: list[str] = [
 
 # Whole-source train/test assignment (used when either list is non-empty).
 TRAIN_SOURCE_IDS: list[str] = [
-    "jZ18INu4LQc",
-    "vq3CZAx3GnM",
+    # "jZ18INu4LQc",
+    # "vq3CZAx3GnM",
     "GRuOSrz3kdY",
     "1rXZJyVXUHU",
     "6rRMEXuLAng",
     "Fr3ue3w5QRY",
     "ANwMhMfcwGM",
     "2crSZaHIBaY",
-]
-TEST_SOURCE_IDS: list[str] = [
     "Y0l5bl2Bp-M",
     "Gzx01gOde80",
     "xDEe3bIX628",
+    # "RCbQVAISMcU",
+    # "TRt7udwisVU",
+]
+TEST_SOURCE_IDS: list[str] = [
+    "jZ18INu4LQc",
+    "vq3CZAx3GnM",
+    # "GRuOSrz3kdY",
+    # "1rXZJyVXUHU",
+    # "6rRMEXuLAng",
+    # "Fr3ue3w5QRY",
+    # "ANwMhMfcwGM",
+    # "2crSZaHIBaY",
+    # "Y0l5bl2Bp-M",
+    # "Gzx01gOde80",
+    # "xDEe3bIX628",
+    # "RCbQVAISMcU",
+    # "TRt7udwisVU",
 ]
